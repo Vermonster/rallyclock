1fb645d595040869b76ce1d97e6dd51d3ac8ec65
Gregory Salmon <salmonthefish@gmail.com>
2012-04-11 07:53:58 -0400

    remove user_id field from group
    not sure why it was there.

--- db/migrations/0003_addgroups.rb
+++ db/migrations/0003_addgroups.rb
@@ -3,7 +3,6 @@ Sequel.migration do
     create_table(:groups) do
       primary_key :id
       String :name
-      foreign_key :user_id, :users
       foreign_key :owner_id, :users
       DateTime :created_at
       DateTime :updated_at
